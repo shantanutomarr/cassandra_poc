675e92d436a3024b24a4620e1d5113f80f579c12
Shantanu Tomar <shantanu@testpress.in>
2024-10-04 16:31:20 +0530

    make fields required

--- sample_app/models.py
+++ sample_app/models.py
@@ -6,6 +6,6 @@ from django_cassandra_engine.models import DjangoCassandraModel
 
 class Book(DjangoCassandraModel):
     id = columns.UUID(primary_key=True, default=uuid.uuid4)
-    title = columns.Text()
-    author = columns.Text()
+    title = columns.Text(required=True)
+    author = columns.Text(required=True)
     published_year = columns.Integer()
